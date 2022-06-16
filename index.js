c7a59a331aeabd97f53487f1c3308bc5b2ec67e0
Abel Bokure <abel.ngt@gmail.com>
2022-06-16 13:28:39 -0400

    commit tree 84524f6c474160c826b1cdaae5b6e3b400c303b2

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = (num1 * num2) 
+function multiply (num1,num2){
+    return multiply
+}
+let random = Math.floor(Math.random()*10)
+function random (){
+return random
+}
+let num4 = 12
+let num3 = 3
+let mod = num4 / num3
+function mod(){
+    return mod
+}
+
+let max = Math.max(1,3,5,10,15,20)
+function max () {
+    return max.ceiling()
+}
